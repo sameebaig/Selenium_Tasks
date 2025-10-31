e50b8a88802a62e2b147e1db0ca634494fd805d0
sameebaig <samee.baig@venturedive.com>
2025-10-31 17:07:49 +0500

    Implement DriverFactory for WebDriver initialization

---
+++ src/main/java/utils/DriverFactory.java
@@ -0,0 +1,38 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactory {
+    private static WebDriver driver;
+
+    public static WebDriver initDriver(String browser) {
+        if (browser == null || browser.isEmpty()) {
+            throw new IllegalArgumentException("Browser is empty");
+        }
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);}
+        driver.manage().window().maximize();
+        return driver;}
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
